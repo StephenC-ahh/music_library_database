3c7a98942561638cf86a7e4684b07796ab0373eb
StephenC-ahh <stephen.coatsworth@sky.com>
2022-09-02 10:15:34 +0100

    post albums implemented

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ class Application < Sinatra::Base
     also_reload 'lib/album_repository'
     also_reload 'lib/artist_repository'
   end
+
   get '/' do
     return erb(:index)
   end
@@ -25,7 +26,31 @@ class Application < Sinatra::Base
     repo = AlbumRepository.new
     @albums = repo.all
     return erb(:get_albums)
+  end  
+
+  get '/albums/new' do
+    return erb(:new_album)
   end
+  
+  post '/albums' do
+    if invalid_request_parameters?
+      status 400
+      return ''
+    end
+    repo = AlbumRepository.new
+    new_album = Album.new
+    new_album.title = params[:title]
+    new_album.release_year = params[:release_year]
+    new_album.artist_id = params[:artist_id]
+    repo.create(new_album)
+    return ''
+  end
+  
+  def invalid_request_parameters?
+    return (params[:title] == nil || params[:release_year] == nil || params[:artist_id] == nil)
+  end
+
+ 
 
   get '/albums/:id' do
     repo = AlbumRepository.new
@@ -35,18 +60,6 @@ class Application < Sinatra::Base
     return erb(:albums)
   end
   
-  post '/albums' do
-    repo = AlbumRepository.new
-    new_album = Album.new
-    new_album.title = params[:title]
-    new_album.release_year = params[:release_year]
-    new_album.artist_id = params[:artist_id]
-
-    repo.create(new_album)
-
-    return ''
-  end
-
   get '/artists' do
     repo = ArtistRepository.new
     @artists = repo.all
@@ -68,6 +81,9 @@ class Application < Sinatra::Base
     return ''
   end
 end
+
+ 
+
 
  # get '/albums' do
   #   repo = AlbumRepository.new
